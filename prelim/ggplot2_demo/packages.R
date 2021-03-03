e07b6f6e45bf997b23702b0221a1eadbf3af64b0
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-03-03 23:17:00 +1300

    load lib

--- prelim/ggplot2_demo/packages.R
+++ prelim/ggplot2_demo/packages.R
@@ -1,10 +1,12 @@
 loadR = function(x) {
+  x = deparse(substitute(x))
   if (!eval(parse(text = paste("require(", x, ")")))) {
-    install.packages(x); library(x)
+    install.packages(x)
+    eval(parse(text = paste("library(", x, ")")))
   }
 }
 
-loadR("magrittr")
-loadR("ggplot2")
-loadR("tsibble")
-loadR("tidyr")
+loadR(magrittr)
+loadR(ggplot2)
+loadR(tsibble)
+loadR(tidyr)
