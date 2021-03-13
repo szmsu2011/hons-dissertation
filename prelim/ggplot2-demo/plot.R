ef89e65fa918fb03323caa481a21c4eeab73815f
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-03-14 08:42:29 +1300

    gg_botseasplot draft

--- prelim/ggplot2-demo/plot.R
+++ prelim/ggplot2-demo/plot.R
@@ -1,32 +1,95 @@
 extract_period <- function(idx, period) {
-  f <-
-    if (period == "day") {
+  if (period == "day") {
+    paste0(
+      sprintf("%02d", lubridate::hour(idx)),
+      ":00"
+    )
+  } else if (period == "week") {
+    lubridate::wday(idx, label = TRUE)
+  } else if (period == "month") {
+    paste0(
+      "W0",
+      (lubridate::day(idx) - 1) %/% 7 + 1
+    )
+  } else if (period == "year") {
+    lubridate::month(idx, label = TRUE)
+  } else {
+    stop("Invalid period")
+  }
+}
+
+
+interval_to_period <- function(interval) {
+  with(
+    interval,
+    lubridate::years(year) +
+      lubridate::period(3 * quarter + month, units = "month") +
+      lubridate::weeks(week) +
+      lubridate::days(day) + lubridate::hours(hour) +
+      lubridate::minutes(minute) +
+      lubridate::seconds(second) +
+      lubridate::milliseconds(millisecond) +
+      lubridate::microseconds(microsecond) +
+      lubridate::nanoseconds(nanosecond)
+  )
+}
+
+
+one_less <- function(period) {
+  case_when(
+    period == "day" ~ "hour",
+    period == "week" ~ "day",
+    period == "month" ~ "week"
+  )
+}
+
+
+period_seq <- function(start, period_step,
+                       break_idx, end_idx) {
+  n <- diff(break_idx)
+
+  c(
+    seq(start, by = period_step, length = n[1]),
+    purrr::as_vector(map(
+      n[-1],
       function(x) {
-        paste0(
-          sprintf("%02d", lubridate::hour(x)),
-          ":00"
-        )
+        seq(1, by = period_step, length = x)
       }
-    } else if (period == "week") {
-      function(x) {
-        lubridate::wday(x, label = TRUE)
-      }
-    } else if (period == "month") {
-      function(x) {
-        paste0(
-          "W0",
-          (lubridate::day(x) - 1) %/% 7 + 1
-        )
-      }
-    } else if (period == "year") {
-      function(x) {
-        lubridate::month(x, label = TRUE)
-      }
+    )),
+    seq(1, by = period_step, length = end_idx - sum(n))
+  )
+}
+
+
+period_as_num <- function(idx, period_chr,
+                          period, ts_interval) {
+  period_discrete <-
+    if (is.na(suppressWarnings(as.numeric(period_chr[1])))) {
+      as.numeric(gsub(":00|W0", "", period_chr))
     } else {
-      stop("Invalid period")
+      as.numeric(period_chr)
     }
 
-  f(idx)
+  if (period == "day") {
+    return(period_discrete - min(period_discrete) + 1)
+  }
+
+  break_idx <- c(1, which(diff(period_discrete) < 0) + 1)
+  period_step <-
+    if (period == "month") {
+      days(days_in_month(1:12))
+    } else {
+      ts_interval /
+        eval(parse(text = paste0(
+          "lubridate::", one_less(period), "s(1)"
+        )))
+    }
+
+  period_seq(
+    period_discrete[1], period_step,
+    break_idx, length(period_discrete)
+  ) %>%
+    (function(x) x - min(x) + 1)()
 }
 
 
@@ -56,18 +119,34 @@ gg_plots <- function(data, y, group_by, ...) {
 }
 
 
-gg_botsplot <- function(data, y, period, ...) {
-  idx <- tsibble::index_var(data)
+gg_botseasplot <- function(data, y, period, ...) {
   data <- data %>%
-    dplyr::mutate(period_time = extract_period(!!sym(idx), period))
+    as_tsibble() %>%
+    tsibble::fill_gaps()
+
+  idx <- tsibble::index_var(data)
+  ts_interval <- interval_to_period(tsibble::interval(data))
+  data <- data %>%
+    dplyr::mutate(
+      period_chr = extract_period(!!sym(idx), period),
+      period_id = period_identifier(!!sym(idx), period),
+      period_num = period_as_num(
+        !!sym(idx), period_chr, period, ts_interval
+      )
+    )
 
   mapping <- aes(
-    x = period_time,
+    x = period_chr,
     y = !!substitute(y)
   )
 
   data %>%
     ggplot(mapping) +
+    geom_line(aes(
+      x = period_num,
+      y = !!substitute(y),
+      group = period_id
+    )) +
     geom_boxplot() +
     xlab("")
 }
