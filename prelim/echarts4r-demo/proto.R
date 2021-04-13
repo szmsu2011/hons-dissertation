4ad4ee355f10f31c44f1a5de9ab994801a9ef4a6
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-04-13 20:03:47 +1200

    interactive ts lineplot

--- prelim/echarts4r-demo/proto.R
+++ prelim/echarts4r-demo/proto.R
@@ -111,3 +111,48 @@ env_data %>%
     )
   ) %>%
   e_tooltip()
+
+#################### {echarts4r} Line-Scatter Linking ####################
+
+loc <- unique(env_data[["location"]])
+
+env_data %>% # Very Slow
+  as_tibble() %>%
+  dplyr::select(datetime, location, airtemp) %>%
+  tidyr::pivot_wider(
+    names_from = location,
+    values_from = airtemp
+  ) %>%
+  e_charts(datetime) %>%
+  e_line_(loc[1], symbol = "none") %>%
+  e_line_(loc[2], x_index = 1, y_index = 1, symbol = "none") %>%
+  e_line_(loc[3], x_index = 2, y_index = 2, symbol = "none") %>%
+  e_line_(loc[4], x_index = 3, y_index = 3, symbol = "none") %>%
+  e_line_(loc[5], x_index = 4, y_index = 4, symbol = "none") %>%
+  e_grid(height = "11%", top = "5%") %>%
+  e_grid(height = "11%", top = "23%") %>%
+  e_grid(height = "11%", top = "41%") %>%
+  e_grid(height = "11%", top = "59%") %>%
+  e_grid(height = "11%", top = "77%") %>%
+  e_y_axis(gridIndex = 0, index = 0) %>%
+  e_x_axis(gridIndex = 0, index = 0) %>%
+  e_y_axis(gridIndex = 1, index = 1) %>%
+  e_x_axis(gridIndex = 1, index = 1) %>%
+  e_y_axis(gridIndex = 2, index = 2) %>%
+  e_x_axis(gridIndex = 2, index = 2) %>%
+  e_y_axis(gridIndex = 3, index = 3) %>%
+  e_x_axis(gridIndex = 3, index = 3) %>%
+  e_y_axis(gridIndex = 4, index = 4) %>%
+  e_x_axis(gridIndex = 4, index = 4) %>%
+  e_datazoom(x_index = 0:4) %>%
+  e_tooltip("axis") %>%
+  e_title("airtemp")
+
+## {plotly} equivalent is much faster
+invisible(purrr::map(paste0(
+  "prelim/ggplot2-demo/",
+  c("packages", "function", "plot"),
+  ".R"
+), source))
+
+gg_plots(env_data, airtemp) %>% plotly::ggplotly()
