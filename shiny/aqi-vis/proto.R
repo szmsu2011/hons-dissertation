649d7da6d1da26f1253aea36005dd7f66232c81c
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-05-19 19:48:12 +1200

    aqi-vis-proto2

--- shiny/aqi-vis/proto.R
+++ shiny/aqi-vis/proto.R
@@ -1,5 +1,4 @@
 library(shiny)
-
 invisible(purrr::map(c(
   paste0(
     "../../prelim/ggplot2-demo/",
@@ -38,13 +37,12 @@ aqi_daily <- aqi_hourly %>%
   as_tsibble(index = date)
 
 ui <- fluidPage(
-  plotlyOutput("p"),
+  plotOutput("p", click = "plot_click"),
   uiOutput("back")
 )
 
 server <- function(input, output, ...) {
   day_range <- unique(aqi_daily[["date"]])
-
   current_day <- reactiveVal()
 
   current_data <- reactive({
@@ -54,44 +52,49 @@ server <- function(input, output, ...) {
     filter(aqi_hourly, date == current_day())
   })
 
-  output[["p"]] <- renderPlotly({
-    intrvl <- feasts:::interval_to_period(interval(current_data()))
-    mapping <-
+  output[["p"]] <- renderPlot(
+    {
+      intrvl <- feasts:::interval_to_period(interval(current_data()))
+      mapping <-
+        if (intrvl == days(1)) {
+          list(aes(mday, month), aes(label = max_aqi))
+        } else {
+          list(aes(hour, date), aes(label = aqi))
+        }
+      p <- current_data() %>%
+        ggplot(mapping[[1]]) +
+        geom_tile(aes(fill = aqi_cat), width = .95, height = .95) +
+        geom_text(mapping[[2]], size = 3) +
+        scale_x_continuous(expand = expansion()) +
+        scale_fill_manual(values = aqi_pal, drop = FALSE) +
+        guides(fill = guide_legend(title = "AQI", nrow = 1)) +
+        theme_bw() +
+        labs(x = "Hour of the Day", y = "Date") +
+        theme(
+          axis.ticks = element_blank(),
+          panel.border = element_blank(),
+          panel.grid = element_blank(),
+          legend.position = "top"
+        )
       if (intrvl == days(1)) {
-        list(aes(mday, month), aes(label = max_aqi))
-      } else {
-        list(aes(hour, date), aes(label = aqi))
+        return(p +
+          labs(x = "Day of the Month", y = "Month") +
+          scale_y_discrete(expand = expansion(), limits = rev)) +
+          coord_fixed(expand = FALSE)
       }
-
-    p <- current_data() %>%
-      ggplot(mapping[[1]]) +
-      geom_tile(aes(fill = aqi_cat), width = .95, height = .95) +
-      geom_text(mapping[[2]], size = 3) +
-      scale_x_continuous(expand = expansion()) +
-      scale_fill_manual(values = aqi_pal, drop = FALSE) +
-      guides(fill = guide_legend(title = "AQI", nrow = 1)) +
-      theme_bw() +
-      labs(x = "Hour of the Day", y = "Date") +
-      theme(
-        axis.ticks = element_blank(),
-        panel.border = element_blank()
-      )
-    if (intrvl == days(1)) {
-      return(p +
-        labs(x = "Day of the Month", y = "Month") +
-        scale_y_discrete(expand = expansion(), limits = rev))
-    }
-    p
-  })
+      p
+    },
+    res = 110
+  )
 
   observe({
-    cd <- event_data("plotly_click")
-    date <- ymd(paste(2019, (12:1)[cd[["y"]]], cd[["x"]]))
-
-    if (!(is.null(cd[["x"]]) | is.na(date))) {
-      current_day(
-        ymd(paste(2019, (12:1)[cd[["y"]]], cd[["x"]]))
-      )
+    cd <- input[["plot_click"]]
+    if (!is.null(cd)) {
+      cd <- map(c("x", "y"), function(x) floor(cd[[x]] + .5))
+    }
+    date <- ymd(paste(2019, (12:1)[cd[[2]]], cd[[1]]))
+    if (!(is.null(cd[[1]]) | is.na(date))) {
+      current_day(date)
     }
   })
 
