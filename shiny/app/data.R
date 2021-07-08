f49d2cc6ee6a5737b8c86738992cc93d12b6fb8e
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-07-08 23:39:03 +1200

    move station to data

--- shiny/app/data.R
+++ shiny/app/data.R
@@ -12,3 +12,18 @@ data <- read_csv("data/akl-env-data.csv",
 
 aqi_data <- select(data, c(aqi, aqi_cat))
 other_data <- select(data, -c(aqi, aqi_cat))
+
+station <- tibble(
+  site = c(
+    "Queen Street", "Glen Eden", "Penrose", "Henderson", "Pakuranga",
+    "Takapuna", "Patumahoe", "Customs St", "Khyber Pass", "Papatoetoe"
+  ),
+  lng = c(
+    174.7650, 174.6447, 174.8199, 174.6239, 174.8709,
+    174.7651, 174.8282, 174.7683, 174.7693, 174.8645
+  ),
+  lat = -c(
+    36.8482, 36.9225, 36.9045, 36.8679, 36.9130,
+    36.7899, 37.2047, 36.8450, 36.8653, 36.9756
+  )
+)
