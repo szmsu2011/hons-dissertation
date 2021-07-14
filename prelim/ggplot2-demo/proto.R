52d2ef8c70861a066671cc02fe19826a23010420
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-07-15 01:46:40 +1200

    wind data viz

--- prelim/ggplot2-demo/proto.R
+++ prelim/ggplot2-demo/proto.R
@@ -18,69 +18,123 @@ import(
 
 wind_dir_data <- "data/akl-wind-dir.csv" %>%
   readr::read_csv(locale = locale(tz = "Pacific/Auckland")) %>%
+  as_tsibble(index = datetime, key = location)
+
+wind_data <- "data/akl-env-data.csv" %>%
+  readr::read_csv(
+    col_types = paste0("Tc", paste(rep("d", 14), collapse = "")),
+    locale = locale(tz = "Pacific/Auckland")
+  ) %>%
   as_tsibble(index = datetime, key = location) %>%
-  dplyr::filter(location == "glen_eden")
+  dplyr::filter(location %in% unique(wind_dir_data[["location"]])) %>%
+  dplyr::select(ws) %>%
+  dplyr::inner_join(wind_dir_data) %>%
+  dplyr::filter(!is.na(ws), !is.na(wind_dir))
 
-## Mean Wind Velocity by Month, Hour of Day, Week of Year and Day of Year
-wind_dir_data %>%
-  as_tibble() %>%
-  dplyr::group_by(Month = month(datetime)) %>%
-  dplyr::summarise(
-    x = vector_mean(wind_dir, 1, 180, 1),
-    y = vector_mean(wind_dir, 1, 180, 2)
-  ) %>%
-  dplyr::mutate(
-    r = sqrt(x^2 + y^2),
-    theta = acos(x / r) * 180 / pi
-  ) %>%
-  ggplot(aes(theta, 0, xend = theta, yend = r, col = Month)) +
-  geom_segment(arrow = arrow(15, length = unit(.04, "npc"))) +
-  ggplot2::scale_x_continuous(breaks = seq(0, 315, 45), limits = c(0, 360)) +
-  ggplot2::scale_colour_gradient2(
-    low = "blue", mid = "red", high = "orange",
-    midpoint = 6.5
-  ) +
-  ggplot2::coord_polar() +
-  ggplot2::labs(y = "ABS(Mean Wind Velocity)")
+akl <- akl_city_grid %>%
+  filter(name %in% unique(wind_data[["location"]])) %>%
+  mutate(row = case_when(row == 3 ~ 2, row == 5 ~ 3, TRUE ~ 1))
 
-wind_dir_data %>%
-  as_tibble() %>%
-  dplyr::group_by(Hour = hour(datetime)) %>%
-  dplyr::summarise(
-    x = vector_mean(wind_dir, 1, 180, 1),
-    y = vector_mean(wind_dir, 1, 180, 2)
-  ) %>%
-  dplyr::mutate(
-    r = sqrt(x^2 + y^2),
-    theta = acos(x / r) * 180 / pi
-  ) %>%
-  ggplot(aes(theta, 0, xend = theta, yend = r, col = Hour)) +
-  geom_segment(arrow = arrow(15, length = unit(.04, "npc"))) +
-  ggplot2::scale_x_continuous(breaks = seq(0, 315, 45), limits = c(0, 360)) +
-  ggplot2::scale_colour_gradient2(
-    low = "blue", mid = "red", high = "orange",
-    midpoint = 12.5
-  ) +
-  ggplot2::coord_polar() +
-  ggplot2::labs(y = "ABS(Mean Wind Velocity)")
-
-wind_dir_data %>%
+wind_data %>%
+  dplyr::filter(location == "takapuna") %>%
   as_tibble() %>%
   dplyr::group_by(WoY = week(datetime)) %>%
   dplyr::summarise(
-    x = vector_mean(wind_dir, 1, 180, 1),
-    y = vector_mean(wind_dir, 1, 180, 2)
+    x = vector_mean(wind_dir, ws, 180, 1),
+    y = vector_mean(wind_dir, ws, 180, 2)
   ) %>%
   dplyr::mutate(
     r = sqrt(x^2 + y^2),
     theta = acos(x / r) * 180 / pi
   ) %>%
   ggplot(aes(theta, 0, xend = theta, yend = r, col = WoY)) +
-  geom_segment(arrow = arrow(15, length = unit(.04, "npc"))) +
+  geom_segment(arrow = arrow(15, length = unit(.02, "npc"))) +
   ggplot2::scale_x_continuous(breaks = seq(0, 315, 45), limits = c(0, 360)) +
   ggplot2::scale_colour_gradient2(
     low = "blue", mid = "red", high = "orange",
     midpoint = 26
   ) +
   ggplot2::coord_polar() +
-  ggplot2::labs(y = "ABS(Mean Wind Velocity)")
+  ggplot2::labs(
+    title = "Average Wind Velocity by Week of Year in Takapuna",
+    x = "Direction",
+    y = "ABS(Mean Wind Velocity)"
+  )
+
+wind_data %>%
+  dplyr::filter(location == "takapuna") %>%
+  as_tibble() %>%
+  dplyr::group_by(Hour = hour(datetime)) %>%
+  dplyr::summarise(
+    x = vector_mean(wind_dir, ws, 180, 1),
+    y = vector_mean(wind_dir, ws, 180, 2)
+  ) %>%
+  dplyr::mutate(
+    r = sqrt(x^2 + y^2),
+    theta = acos(x / r) * 180 / pi
+  ) %>%
+  ggplot(aes(theta, 0, xend = theta, yend = r, col = Hour)) +
+  geom_segment(arrow = arrow(15, length = unit(.02, "npc"))) +
+  ggplot2::scale_x_continuous(breaks = seq(0, 315, 45), limits = c(0, 360)) +
+  ggplot2::scale_colour_gradient2(
+    low = "blue", mid = "red", high = "orange",
+    midpoint = 12.5
+  ) +
+  ggplot2::coord_polar() +
+  ggplot2::labs(
+    title = "Average Wind Velocity by Hour of Day in Takapuna",
+    x = "Direction",
+    y = "ABS(Mean Wind Velocity)"
+  )
+
+wind_data %>%
+  as_tibble() %>%
+  dplyr::group_by(WoY = week(datetime), location) %>%
+  dplyr::summarise(
+    x = vector_mean(wind_dir, ws, 180, 1),
+    y = vector_mean(wind_dir, ws, 180, 2)
+  ) %>%
+  dplyr::mutate(
+    r = sqrt(x^2 + y^2),
+    theta = acos(x / r) * 180 / pi
+  ) %>%
+  ggplot(aes(theta, 0, xend = theta, yend = r, col = WoY)) +
+  geom_segment(arrow = arrow(15, length = unit(.02, "npc"))) +
+  ggplot2::scale_x_continuous(breaks = seq(0, 315, 45), limits = c(0, 360)) +
+  ggplot2::scale_colour_gradient2(
+    low = "blue", mid = "red", high = "orange",
+    midpoint = 26
+  ) +
+  facet_geo(~location, grid = akl) +
+  ggplot2::coord_polar() +
+  ggplot2::labs(
+    title = "Average Wind Velocity by Week of Year",
+    x = "Direction",
+    y = "ABS(Mean Wind Velocity)"
+  )
+
+wind_data %>%
+  as_tibble() %>%
+  dplyr::group_by(Hour = hour(datetime), location) %>%
+  dplyr::summarise(
+    x = vector_mean(wind_dir, ws, 180, 1),
+    y = vector_mean(wind_dir, ws, 180, 2)
+  ) %>%
+  dplyr::mutate(
+    r = sqrt(x^2 + y^2),
+    theta = acos(x / r) * 180 / pi
+  ) %>%
+  ggplot(aes(theta, 0, xend = theta, yend = r, col = Hour)) +
+  geom_segment(arrow = arrow(15, length = unit(.02, "npc"))) +
+  ggplot2::scale_x_continuous(breaks = seq(0, 315, 45), limits = c(0, 360)) +
+  ggplot2::scale_colour_gradient2(
+    low = "blue", mid = "red", high = "orange",
+    midpoint = 12.5
+  ) +
+  facet_geo(~location, grid = akl) +
+  ggplot2::coord_polar() +
+  ggplot2::labs(
+    title = "Average Wind Velocity by Hour of Day",
+    x = "Direction",
+    y = "ABS(Mean Wind Velocity)"
+  )
