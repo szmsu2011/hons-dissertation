b652a22a7314bf14f70edd3863f47dbcb8499d59
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-07-16 04:27:55 +1200

    reduces repetitive loading

--- shiny/app/data.R
+++ shiny/app/data.R
@@ -1,17 +1,32 @@
-data <- read_csv("data/akl-env-data.csv",
-  col_types = paste0("Tc", paste(rep("d", 14), collapse = "")),
-  locale = locale(tz = "Pacific/Auckland")
-) %>%
-  mutate(
-    datetime = as_datetime(datetime) + hours(12),
-    aqi_cat = aqi_cat(aqi),
-    hour = hour(datetime)
-  ) %>%
-  filter(year(datetime) > 2015) %>%
-  as_tsibble(index = datetime, key = location)
+require_load <- function(x, md5) {
+  if (exists(x)) {
+    x <- eval(sym(x))
+    as.character(openssl::md5(serialize(x, NULL))) != md5
+  } else {
+    TRUE
+  }
+}
 
-aqi_data <- select(data, c(aqi, aqi_cat))
-con_data <- select(data, aqi, !!sym("pm2.5"), pm10, no2, so2, co, o3)
+if (require_load("data", "b72ec360895f0babbf84d43b6cc87c74")) {
+  data <- read_csv("data/akl-env-data.csv",
+    col_types = paste0("Tc", paste(rep("d", 14), collapse = "")),
+    locale = locale(tz = "Pacific/Auckland")
+  ) %>%
+    mutate(
+      datetime = as_datetime(datetime) + hours(12),
+      aqi_cat = aqi_cat(aqi),
+      hour = hour(datetime)
+    ) %>%
+    filter(year(datetime) > 2015) %>%
+    as_tsibble(index = datetime, key = location)
+}
+
+if (require_load("aqi_data", "15e0b7480c010a3db774ac801a34fcc6")) {
+  aqi_data <- select(data, c(aqi, aqi_cat))
+}
+if (require_load("con_data", "fb838b5ede632f3cde868948659a289b")) {
+  con_data <- select(data, aqi, !!sym("pm2.5"), pm10, no2, so2, co, o3)
+}
 
 station <- tibble(
   site = c(
