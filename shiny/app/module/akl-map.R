e84e71bca51c90b8f7f4e03fa718258cf8dd2b95
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-07-19 00:43:12 +1200

    map using leaflet

--- shiny/app/module/akl-map.R
+++ shiny/app/module/akl-map.R
@@ -1,27 +1,20 @@
 map_ui <- function(id) {
   ns <- NS(id)
 
-  tagList(mapboxerOutput(ns("map"), height = "1150px"))
+  tagList(leafletOutput(ns("map"), height = "1150px"))
 }
 
 map_mod <- function(id, state) {
   module <- function(input, output, session) {
-    output[["map"]] <- renderMapboxer({
-      station %>%
-        as_mapbox_source() %>%
-        mapboxer(
-          center = c(174.7945, -37.2109),
-          zoom = 9.2
-        ) %>%
-        add_navigation_control() %>%
-        add_circle_layer(
-          circle_color = "white",
-          circle_blur = .5
-        )
+    output[["map"]] <- renderLeaflet({
+      leaflet(data = station) %>%
+        addTiles() %>%
+        setView(174.7945, -37.2109, zoom = 9.5) %>%
+        addMarkers(~lng, ~lat, ~site, label = ~site)
     })
 
-    observeEvent(input[["map_onclick"]], {
-      state[["map_onclick"]] <- input[["map_onclick"]][["props"]][["site"]]
+    observeEvent(input[["map_marker_click"]], {
+      state[["map_onclick"]] <- input[["map_marker_click"]][["id"]]
       state[["aqi_heatmap_datazoom"]] <- NULL
     })
   }
