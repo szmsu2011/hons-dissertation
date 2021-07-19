37fe3598e6d5b5381db295e01a11260ab78cc91a
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-07-19 23:43:08 +1200

    new shinydashboard tabs

--- shiny/app/ui.R
+++ shiny/app/ui.R
@@ -1,15 +1,20 @@
 app_ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
+  dashboardHeader(title = "AKL Environment"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("Air Quality", tabName = "aqi", icon = icon("dashboard")),
+    menuItem("Weather", tabName = "weather", icon = icon("cloud-sun"))
+  )),
   dashboardBody(
     shinyDashboardThemes(theme = "blue_gradient"),
-    fluidPage(
-      fluidRow(selectInput("year", "Year", "")),
-      fluidRow(
-        column(map_ui("map"), width = 4),
-        column(aqi_heatmap_ui("aqi_heatmap"), width = 4),
-        column(aqi_details_ui("aqi_details"), width = 4)
-      )
+    tabItems(
+      tabItem("aqi", fluidPage(
+        fluidRow(selectInput("year", "Year", "")),
+        fluidRow(
+          column(map_ui("map"), width = 4),
+          column(aqi_heatmap_ui("aqi_heatmap"), width = 4),
+          column(aqi_details_ui("aqi_details"), width = 4)
+        )
+      ))
     )
   )
 )
