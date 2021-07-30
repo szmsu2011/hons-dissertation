d744b4455ce7aaf383987474ed7e62cc5bee7c4b
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-07-31 00:35:18 +1200

    adjust wind map centre

--- shiny/app/module/akl-map-wind.R
+++ shiny/app/module/akl-map-wind.R
@@ -1,7 +1,7 @@
 map_wind_ui <- function(id) {
   ns <- NS(id)
 
-  tagList(leafletOutput(ns("map_wind"), height = "2360px"))
+  tagList(leafletOutput(ns("map_wind"), height = "1010px"))
 }
 
 map_wind_mod <- function(id, state) {
@@ -9,7 +9,7 @@ map_wind_mod <- function(id, state) {
     output[["map_wind"]] <- renderLeaflet({
       leaflet(data = station) %>%
         addTiles() %>%
-        setView(174.7485, -37.8474, zoom = 9.5) %>%
+        setView(174.7485, -37.1124, zoom = 9.5) %>%
         addMarkers(~lng, ~lat, ~site, label = ~site)
     })
 
